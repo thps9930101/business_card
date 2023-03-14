68380f04676c9a09715490e6363d8d90f4413c8b
admin <david.pointlab@gmail.com>
2023-03-14 12:31:01 +0000

    增加登入icon和default encryption

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'email' => env('SYSTEM_ADMIN'),
             'is_admin'=>1,
-            'password'=>env('SYSTEM_PASSWORD')
+            'password'=> Hash::make( env('SYSTEM_PASSWORD'))
         ]);
 
         \App\Models\User::factory(10)->create();
