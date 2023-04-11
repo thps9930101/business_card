42ccec395e4196d0559fa75ebbd0d54a1ed5adbf
admin <david.pointlab@gmail.com>
2023-04-11 08:48:56 +0000

    adding backend gate

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -41,7 +41,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return $user->is_admin;
+            return $user->is_admin || ($user->is_store_admin && $user->store()->exists()) ;
         });
     }
 
