568e56997c6ba79a0a0e8c668531957313a7a6f9
admin <david.pointlab@gmail.com>
2023-04-12 03:12:57 +0000

    adding phone attribute to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
             $table->string('password');
             //user types tiny integer
             $table->boolean('is_admin')->default(false);
