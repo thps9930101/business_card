f301ba274c51e1eb105a3d8e1eba97ce807cabf5
admin <david.pointlab@gmail.com>
2023-04-12 03:15:04 +0000

    adding casting to phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -38,6 +39,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'confirm_code_expired_at'=>'datetime'
     ];
+
+    /**
+     * Interact with the user's first name.
+     */
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => str_replace('-','',$value),//format can be either 09xx-xxx-xxx or 09xxxxxxxx and should be cast to 09xxxxxxxx
+            set: fn (string $value) => str_replace('-','',$value),//format can be either 09xx-xxx-xxx or 09xxxxxxxx and should be cast to 09xxxxxxxx
+        );
+    }
 
     /**
      * has many devices
