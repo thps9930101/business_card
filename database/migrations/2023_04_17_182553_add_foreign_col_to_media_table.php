be7766627fd38f4e2b54aee475a18d1fff0da6ff
admin <david.pointlab@gmail.com>
2023-04-20 01:03:47 +0000

    adding media device table relationship

---
+++ database/migrations/2023_04_17_182553_add_foreign_col_to_media_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('media', function (Blueprint $table) {
+            //forign device id
+            $table->foreignId('device_id')->nullable()->constrained()->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('media', function (Blueprint $table) {
+            //
+            $table->drop('device_id');
+        });
+    }
+};
