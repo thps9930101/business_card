ad5e04aa985ea61c7e2bdb17cdb3b766b3c1bfbb
Pointlab developer <david.pointlab@gmail.com>
2023-06-16 07:51:36 +0000

    remove the path and update pulling permission

--- app/Jobs/GitPull.php
+++ app/Jobs/GitPull.php
@@ -56,7 +56,7 @@ class GitPull implements ShouldQueue
         }
 
          // Pull
-         putenv('PATH=/usr/local/bin');
+         /* putenv('PATH=/usr/local/bin'); */
          $process = new Process(['./pulling.sh']);
          $process->setWorkingDirectory(base_path());
          $process->run();
