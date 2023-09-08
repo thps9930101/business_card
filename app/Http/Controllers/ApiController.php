27ee640d6880f56377de1d706f9a04af714eea31
Majjor140 <o47789963321556622167@gmail.com>
2023-09-08 13:28:46 +0800

    Max Upadte.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -345,7 +345,7 @@ class ApiController extends Controller
     public function uploadVideo(Request $request){
 
                 $validator = Validator::make($request->all(),[
-                    'video' => 'required|mimes:mp4,mov,ogg,qt|max:10485760',
+                    'video' => 'required|mimes:mp4,mov,ogg,qt', // |max:104857600
                 ]);
 
                 if($validator->fails()){
