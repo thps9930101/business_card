2fbdccef1288d6265ed8b1189c8887ab9fe2ffb6
Majjor140 <o47789963321556622167@gmail.com>
2023-09-20 15:39:00 +0800

    Test.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,9 +29,9 @@ class UserController extends Controller
     }
 
     public function addDevice(){
-        $user = auth()->user();
-        // event(new AddDevice($user, request('deviceId')));
-        Log::info('Device added', ['user'=>$user->id, 'device'=>request('deviceId')]);
+        /* $user = auth()->user();
+        event(new AddDevice($user, request('deviceId')));
+        Log::info('Device added', ['user'=>$user->id, 'device'=>request('deviceId')]); */
         return [     
             'success'=>true,
             'message'=>'Device added',
