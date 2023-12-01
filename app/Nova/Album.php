137070381c5aa274069c3212a5c5ebde1feffde8
Majjor140 <o47789963321556622167@gmail.com>
2023-12-01 15:51:27 +0800

    Album add to product updated.

--- app/Nova/Album.php
+++ app/Nova/Album.php
@@ -29,7 +29,7 @@ class Album extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'name';
 
     /**
      * The columns that should be searched.
