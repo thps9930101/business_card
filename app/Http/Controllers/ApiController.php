7179bc2a0a4f6554525c93025e6007834967db25
Majjor140 <o47789963321556622167@gmail.com>
2023-12-19 20:49:14 +0800

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -898,6 +898,7 @@ class ApiController extends Controller
                     else{
                         $album = $order->product_solution_order?collect([$order->product_solution_order->product_solution->product->album]) : null;
                         $albumCollection = new AlbumCollection($album);
+                        return $albumCollection;
                         $tmp_media = $albumCollection? $albumCollection->first()->media : null;
                     }
                     array_push($media_arr, $tmp_media);
