b356cc7b0260ac7fd2fb27273ab864066d58227b
Majjor140 <o47789963321556622167@gmail.com>
2023-12-21 13:48:58 +0800

    test.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -909,7 +909,7 @@ class ApiController extends Controller
                 
         
                 foreach ($media_arr as $temp_arr) {
-                    return $temp_arr;
+                    
                     foreach ($temp_arr as $temp) {
                         if ($temp->id == $media->id) {
                             $can_view = true;
