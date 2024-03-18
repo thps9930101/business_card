dcfbeaaead4afd082cc8c009f9402444afe428dc
Majjor140 <o47789963321556622167@gmail.com>
2024-03-18 18:16:05 +0800

    test pusher collection

--- app/Events/PicUploaded.php
+++ app/Events/PicUploaded.php
@@ -37,7 +37,8 @@ class PicUploaded  implements ShouldBroadcastNow
         $this->obj=isset($video->obj)?Storage::disk('s3')->temporaryUrl($video->original??$video->obj, now()->addHour()) : 'null';
         $this->path=isset($vide->order)?(new OrderRepository($video->order))->getPath($video->id) : 'null';
 
-        $this->mediaCollection = MediaResource::collection(Media::Where('id', $video->id)->latest()->get());
+        $ss = MediaResource::collection(Media::Where('id', $video->id)->latest()->get());
+        $this->mediaCollection = "";
     }
 
     /**
