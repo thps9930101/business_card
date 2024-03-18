ee2ee65f4a198856f5c27a0c31082f359a2c43d9
Majjor140 <o47789963321556622167@gmail.com>
2024-03-18 21:39:22 +0800

    just try

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'https://4dbox.lightmatrix3d.com/api/*'//
+        '*'//https://4dbox.lightmatrix3d.com/api/*
     ];
 }
