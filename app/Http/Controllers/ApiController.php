ad6b7e1565eccaea31b642e16444ff96017c3744
Majjor140 <o47789963321556622167@gmail.com>
2024-03-19 13:06:15 +0800

    remove user 1 auto remove

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -561,8 +561,9 @@ class ApiController extends Controller
             $user->$target += (int)$request->value;
             $user->save();
 
-            if ($media->user_id == 598 || $media->user_id == 1)
+            if ($media->user_id == 598)
             {
+                // || $media->user_id == 1
                 AutoDeleteGuestMedia::dispatch($media->id)->delay(now()->addMinutes(10));
                 // AutoDeleteGuestMedia::dispatch($media->id)->delay(now()->addSeconds(30));
             }
