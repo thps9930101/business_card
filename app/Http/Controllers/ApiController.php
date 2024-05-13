cd5819468636b96ec2e5ee59152051d352ec1830
thps9930101 <thps9930101@gmail.com>
2024-05-13 18:43:46 +0800

    addMaterials save tmp file

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -586,20 +586,34 @@ class ApiController extends Controller
         $filePath = env('APP_ENV')."/".$user->id.'/'.'material/'.$request->card_url->getClientOriginalName();
 
         $tmpFilePath = sys_get_temp_dir() . '/' . uniqid() . '.png';
+        file_put_contents($tmpFilePath, ''); 
+        if (file_exists($tmpFilePath)) {
+            $file = new \Illuminate\Http\UploadedFile($tmpFilePath, 'image.png', 'image/png', null, true);
+            $file->store($filePath, 's3');
+    
+            $material = new materials();
+            $material->user_id = $user->id;
+            $material->card_url = $filePath;
+            $material->save();
+
+            unlink($tmpFilePath);
+            return [
+                'success' => true,
+                'message' => [
+                ],
+            ];
+        }
+        else {
+            return [
+                'success' => false,
+                'message' => [
+                ],
+            ];
+        }
+
         // $image = Image::make($request->card_url);
         // $image->save($tmpFilePath);
-        $file = new \Illuminate\Http\UploadedFile($tmpFilePath, 'image.png', 'image/png', null, true);
-        $file->store($filePath, 's3');
 
-        $material = new materials();
-        $material->user_id = $user->id;
-        $material->card_url = $filePath;
-        $material->save();
-        return [
-            'success' => true,
-            'message' => [
-            ],
-        ];
         
         return[
             'filename:'=>$request->card_url->getClientOriginalName()
