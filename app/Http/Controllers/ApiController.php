4473941cf7a80d4b1d7d7233f4e8a045c0fc4463
thps9930101 <thps9930101@gmail.com>
2024-05-13 19:21:30 +0800

    fix getBC condition error

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -779,12 +779,12 @@ class ApiController extends Controller
         $msg = $getBC_Result['success']['message'];
         if ($msg["card_front"] != '')
             Storage::disk('s3')->temporaryUrl($msg["card_front"], now()->addHour());
-        if ($msg["card_back"] == '')
+        if ($msg["card_back"] != '')
             Storage::disk('s3')->temporaryUrl($msg["card_back"], now()->addHour());
 
-        if ($msg["model"]["texture"] == '')
+        if ($msg["model"]["texture"] != '')
             Storage::disk('s3')->temporaryUrl($msg["model"]["texture"], now()->addHour());
-        if ($msg["model"]["mesh"] == '')
+        if ($msg["model"]["mesh"] != '')
             Storage::disk('s3')->temporaryUrl($msg["model"]["mesh"], now()->addHour());
         
         foreach($social_array as $name => $social)
