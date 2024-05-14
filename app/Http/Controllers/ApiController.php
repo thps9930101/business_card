4aeeb79a163322e3cbd2625236b0fc06fe16ebc6
thps9930101 <thps9930101@gmail.com>
2024-05-14 13:17:11 +0800

    test editBC

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -518,7 +518,7 @@ class ApiController extends Controller
         $card_info = $request->input('card');
         $user = Auth::user();
 
-        $card = cards::where('user_id', $BC_id);
+        $card = cards::where('id', $BC_id);
 
         if($card->user_id != $user->id)
         {
