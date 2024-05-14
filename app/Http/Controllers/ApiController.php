b4b5d00315c1637ab85c789df7e7ed56356e3af0
thps9930101 <thps9930101@gmail.com>
2024-05-14 15:21:26 +0800

    fix struct error

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -538,23 +538,23 @@ class ApiController extends Controller
         Log::info("card model_id: ".$card->model_id);
 
         // TODO 檢查該 各個ID 是否為該 user 的 ID
-        $card->name = $card_info->name ?? $card->name;
-        $card->email = $card_info->email ?? $card->email;
-        $card->phone = $card_info->phone ?? $card->phone;
-        $card->address = $card_info->address ?? $card->address;
-        $card->fax = $card_info->fax ?? $card->fax;
-        $card->edit_name = $card_info->edit_name ?? $card->edit_name;
-        $card->release_name = $card_info->release_name ?? $card->release_name;
-        $card->model_id = $card_info->model_id ?? $card->model_id;
-        $card->card_front_id = $card_info->card_front_id ?? $card->card_front_id;
-        $card->card_back_id = $card_info->card_back_id ?? $card->card_back_id;
-        $card->telegram = $card_info->telegram ?? $card->telegram;
-        $card->whatsapp = $card_info->whatsapp ?? $card->whatsapp;
-        $card->facebook = $card_info->facebook ?? $card->facebook;
-        $card->instagram = $card_info->instagram ?? $card->instagram;
-        $card->X = $card_info->X ?? $card->X;
-        $card->web = $card_info->web ?? $card->web;
-        $card->is_actived = $card_info->is_actived ?? $card->is_actived;
+        $card->name = $card_info['name'] ?? $card->name;
+        $card->email = $card_info['email'] ?? $card->email;
+        $card->phone = $card_info['phone'] ?? $card->phone;
+        $card->address = $card_info['address'] ?? $card->address;
+        $card->fax = $card_info['fax'] ?? $card->fax;
+        $card->edit_name = $card_info['edit_name'] ?? $card->edit_name;
+        $card->release_name = $card_info['release_name'] ?? $card->release_name;
+        $card->model_id = $card_info['model_id'] ?? $card->model_id;
+        $card->card_front_id = $card_info['card_front_id'] ?? $card->card_front_id;
+        $card->card_back_id = $card_info['card_back_id'] ?? $card->card_back_id;
+        $card->telegram = $card_info['telegram'] ?? $card->telegram;
+        $card->whatsapp = $card_info['whatsapp'] ?? $card->whatsapp;
+        $card->facebook = $card_info['facebook'] ?? $card->facebook;
+        $card->instagram = $card_info['instagram'] ?? $card->instagram;
+        $card->X = $card_info['X'] ?? $card->X;
+        $card->web = $card_info['web'] ?? $card->web;
+        $card->is_actived = $card_info['is_actived'] ?? $card->is_actived;
 
         // foreach ($card_info as $key => $info) {
         //     $card->$key = $info;
