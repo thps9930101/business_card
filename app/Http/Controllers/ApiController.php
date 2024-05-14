693477128347878494635795cc66e56592d1b3b0
thps9930101 <thps9930101@gmail.com>
2024-05-14 15:12:41 +0800

    test editbc debug log

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -533,7 +533,8 @@ class ApiController extends Controller
         //         ]
         //     ];
         // }        
-        Log::info("card model_id: ".$card->model_id.", card_info model_id: ".$card_info->model_id);
+        Log::info("card_info model_id: ".$card_info->model_id);
+        Log::info("card model_id: ".$card->model_id);
 
         // TODO 檢查該 各個ID 是否為該 user 的 ID
         $card->name = $card_info->name ?? $card->name;
